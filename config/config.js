ab396d6eeac2048e52f196f4f8af34cdf2a02a87
Gustavo Crisosth <favocrisosth5@gmail.com>
2026-02-20 09:10:14 -0300

    Usa URL interna do Render sem SSL

--- config/config.js
+++ config/config.js
@@ -14,11 +14,6 @@ module.exports = {
     production: {
         use_env_variable: "DATABASE_URL",
         dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false // <-- Isso avisa ao Node para não barrar o certificado do Render
-            }
-        }
+        dialectOptions: {}
     }
 };
